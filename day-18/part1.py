2cd51b7fb5bafb1e9e2632e1cae11f4e55c0dd6e
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-12-22 21:17:53 +0000

    Advent of Code - Day 18
    And we carry on...
    
    Part 1 and Part 2 were simply solved by bfs and then just completing an iterative board approach that updates and recalculates the board bfs with each coord update.

---
+++ day-18/part1.py
@@ -0,0 +1,38 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+from parser import Parser
+from collections import deque
+
+def print_board(board):
+    for row in board:
+        print(''.join(row))
+
+def bfs(board, y, x):
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    seen = set()
+    queue = deque([(y, x, 0)])
+    bottom_pos = (len(board) - 1, len(board[0]) - 1)
+    while queue:
+        y, x, count = queue.popleft()
+        if (y, x) == bottom_pos:
+            return count
+        if y < 0 or y >= len(board) or x < 0 or x >= len(board[0]):
+            continue
+        if board[y][x] == '#' or (y, x) in seen:
+            continue
+        seen.add((y, x))
+        for dy, dx in directions:
+            queue.append((y + dy, x + dx, count + 1))
+    return -1
+
+def populate_board(board, coords):
+    for x, y in coords:
+        board[y][x] = '#'
+    return board
+
+if __name__ == "__main__":
+    coords = Parser(file_path='input.txt').parse_lines(int, delimiter=',')
+    board = [['.' for _ in range(71)] for _ in range(71)]
+    board = populate_board(board, coords[:1024])
+    print("Step Count:", bfs(board, 0, 0))
